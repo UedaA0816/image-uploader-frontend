009125d837dbff3bf217dca9b220a87048c133f0
ueda <>
2021-08-24 18:41:57 +0900

    add ベース

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export const ImageUploader:React.VFC = ()=>{
   return (
-    <div>
-      imageuploader
+    <div className="bg-white shadow-xl rounded-lg px-7 py-8">
+      imageuploader<br/>
+      imageuploader<br/>
+      imageuploader<br/>
+      imageuploader<br/>
     </div>
   )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,8 +4,9 @@ import { ImageUploader } from "../../components/ImageUploader"
 const Home:React.FC = () => {
     
   return (
-    <div>
+    <div className="bg-gray-100 h-screen flex flex-col justify-center items-center">
       <ImageUploader/>
+      <span className="absolute bottom-6 text-sm font-medium text-gray-500 w-full text-center">created by ueda akihiro - devChallenges.io</span>
     </div>
   )
 }
